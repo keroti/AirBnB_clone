4b311805a26723067ff3a799189bf855c1f6706a
keroti <peterkeroti55@gmail.com>
2022-11-25 01:28:10 -0800

    Base model

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,7 @@
 """ Class BaseModel """
 from datetime import datetime
 from uuid import uuid4
+import models
 
 
 class BaseModel:
@@ -28,6 +29,7 @@ class BaseModel:
             self.id = str(uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
+            models.storage.new(self)
 
     def __str__(self):
         """ String """
@@ -37,6 +39,7 @@ class BaseModel:
     def save(self):
         """ save function """
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """ Return a dictionary """
