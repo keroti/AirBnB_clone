dec46536143c9713de92061cbbcda92a2c3e12b5
keroti <59761453+keroti@users.noreply.github.com>
2022-11-27 07:01:14 +0300

    Test review

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -4,7 +4,7 @@ from tests.test_models.test_base_model import TestBaseModel
 from models.review import Review
 
 
-class test_review(test_basemodel):
+class test_review(TestBaseModel):
     """ """
 
     def __init__(self, *args, **kwargs):
